c36471b57c2473783c2508147e675a54433c94ec
Federico De Ponte <depontefede@gmail.com>
2026-05-01 20:12:13 +0200

    Allow Google profile images in CSP

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const securityHeaders = [
       "base-uri 'self'",
       "object-src 'none'",
       "frame-ancestors 'none'",
-      "img-src 'self' data: blob:",
+      "img-src 'self' data: blob: https://*.googleusercontent.com",
       "font-src 'self' data:",
       "style-src 'self' 'unsafe-inline'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
